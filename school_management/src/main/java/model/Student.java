46122d1e66267dc8815ad8bf070431140849656d
ADuuLayy <118297619+ADuuLayy@users.noreply.github.com>
2024-04-11 14:45:30 +0630

    Latest update
    classroom, student, teacher and grade classes

--- school_management/src/main/java/model/Student.java
+++ school_management/src/main/java/model/Student.java
@@ -1,5 +1,45 @@
 package model;
 
-public class Student {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "student")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "student_id")
+	private int studentId;
+	
+	@OneToOne
+	@JoinColumn(referencedColumnName = "person_id")
+	private PersonInfo personInfo;
+	
+	@OneToOne
+	@JoinColumn(referencedColumnName = "classroom_id")
+	private Classroom classroom;
+	
+	@OneToOne
+	@JoinColumn(referencedColumnName = "grade_id")
+	private Grade grade;
+
+	public Student(int studentId, PersonInfo personInfo, Classroom classroom, Grade grade) {
+		super();
+		this.studentId = studentId;
+		this.personInfo = personInfo;
+		this.classroom = classroom;
+		this.grade = grade;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", personInfo=" + personInfo + ", classroom=" + classroom
+				+ ", grade=" + grade + "]";
+	}
 }
--- school_management/src/main/java/model/Teacher.java
+++ school_management/src/main/java/model/Teacher.java
@@ -1,5 +1,49 @@
 package model;
 
-public class Teacher {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "teacher")
+public class Teacher {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "teacher_id")
+	private int teacherId;
+	
+	@Column(name = "subject")
+	private String subject;
+	
+	@Column(name = "greade_id")
+	private int gradeId;
+	
+	@OneToOne
+	@JoinColumn(referencedColumnName = "person_id")
+	private PersonInfo personInfo;
+	
+	public Teacher(int teacherId, String subject, int gradeId, PersonInfo personInfo) {
+		super();
+		this.teacherId = teacherId;
+		this.subject = subject;
+		this.gradeId = gradeId;
+		this.personInfo = personInfo;
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [teacherId=" + teacherId + ", subject=" + subject +  ", gradeId="
+				+ gradeId + ", personInfo=" + personInfo + "]";
+	}
+	
 }
+
+
